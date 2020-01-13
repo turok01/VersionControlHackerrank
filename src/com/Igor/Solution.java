0c8d8809020ddad0972baaebf00b7b2151c03978
turok01 <44950560+turok01@users.noreply.github.com>
2020-01-13 17:31:49 +0300

    Write empty class MyRegex

--- src/com/Igor/Solution.java
+++ src/com/Igor/Solution.java
@@ -4,15 +4,15 @@ import java.util.regex.Pattern;
 import java.util.Scanner;
 
 class Solution{
-
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         while(in.hasNext()){
             String IP = in.next();
             System.out.println(IP.matches(new MyRegex().pattern));
         }
-
     }
 }
-
+class MyRegex{
+    public String pattern="";
+        }
 //Write your code here
