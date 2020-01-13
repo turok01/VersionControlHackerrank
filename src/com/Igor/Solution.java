414bd4cd0b86cbf94484200d57ac1385dba41f6f
turok01 <44950560+turok01@users.noreply.github.com>
2020-01-13 16:38:45 +0300

    New task BigInteger

--- src/com/Igor/Solution.java
+++ src/com/Igor/Solution.java
@@ -1,57 +1,27 @@
-package com.Igor.;
+package com.Igor;
 import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
 import java.util.*;
-import java.util.stream.IntStream;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
-interface PerformOperation {
-    boolean check(int a);
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String n = scanner.nextLine();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        BigInteger a = new BigInteger(n);
+        if(a.isProbablePrime(1))
+            System.out.println("prime");
+        else
+            System.out.println("not prime");
+
+        scanner.close();
+    }
 }
-class MyMath {
-    public static boolean checker(PerformOperation p, int num) {
-        return p.check(num);
-    }
-    static PerformOperation isOdd(){
-        return (int n) -> n%2==0;
-    }
-    static PerformOperation isPrime(){
-        return (int n)-> IntStream.range(2,(int)Math.sqrt(n+1)).noneMatch(i->n%i==0);
-    }
-    
-}
-
-
-    public  class Solution {
-
-        public static void main(String[] args) throws IOException {
-            //PerformOperation isOdd;
-            //isOdd = a->a%2==0;
-
-            MyMath ob = new MyMath();
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            int T = Integer.parseInt(br.readLine());
-            PerformOperation op;
-            boolean ret = false;
-            String ans = null;
-            while (T--> 0) {
-                String s = br.readLine().trim();
-                StringTokenizer st = new StringTokenizer(s);
-                int ch = Integer.parseInt(st.nextToken());
-                int num = Integer.parseInt(st.nextToken());
-                if (ch == 1) {
-                    op = ob.isOdd();
-                    ret = ob.checker(op, num);
-                    ans = (ret) ? "ODD" : "EVEN";
-                } else if (ch == 2) {
-                    op = ob.isPrime();
-                    ret = ob.checker(op, num);
-                    ans = (ret) ? "PRIME" : "COMPOSITE";
-                } else if (ch == 3) {
-                    op = ob.isPalindrome();
-                    ret = ob.checker(op, num);
-                    ans = (ret) ? "PALINDROME" : "NOT PALINDROME";
-
-                }
-                System.out.println(ans);
-            }
-        }
-    }
