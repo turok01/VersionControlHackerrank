a2933b2eee43b21325ac6c19d464f562142e482c
turok01 <44950560+turok01@users.noreply.github.com>
2020-01-14 17:28:28 +0300

    Junit learning https://javarush.ru/groups/posts/605-junit

--- src/com/Igor/Solution.java
+++ src/com/Igor/Solution.java
@@ -1,8 +1,93 @@
 package com.Igor;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.Scanner;
+//https://javarush.ru/groups/posts/605-junit
+import java.util.*;
 
+class User{
+    private int id;
+    private String name;
+    private int age;
+    private Sex sex;
+    private static Map<Integer,User> allUsers;
+    private static int countId = 0;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public User(int age, String name,Sex sex){
+        if(allUsers==null){
+            allUsers = new HashMap<>();
+        }
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+        if(!hasUser()){
+            countId++;
+            this.id = countId;
+            allUsers.put(id, this);
+        }
+    }
+    public static List<User> getAllUser(){
+        return new ArrayList<>(allUsers.values());
+    }
+    public static List<User> getAllUsers(Sex sex){
+        List<User> listAllUsers = new ArrayList<>();
+        for(User user : allUsers.values()){
+            if(user.sex==sex)
+                listAllUsers.add(user);
+        }
+        return  listAllUsers;
+    }
+    @Override
+    public int hashCode(){
+        return Objects.hash(name, age, sex);
+    }
+    @Override
+    public boolean equals(Object o){
+        if (this==o) return true;
+        if(o==null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age && Objects.equals(name, user.name) && sex==user.sex;
+    }
+    private boolean hasUser(){
+        for(User user : allUsers.values()) {
+            if (user.equals(this) && user.hashCode() == this.hashCode())
+                return true;
+        }
+        return false;
+    }
+    @Override
+    public String toString(){
+        return "User{" +
+                "id=" + id +
+                ", name" + name + '\'' +
+                ", age" + age +
+                ", sex" + sex +
+                '}';
+    }
+}
+class Sex{
+    private String sex;
+
+    public String getSex() {
+        return sex;
+    }
+    public Sex(String sex){
+        this.sex=sex;
+    }
+}
 class Solution{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
@@ -12,8 +97,4 @@ class Solution{
         }
     }
 }
-class MyRegex{
-    public String pattern0="([01]\\d{2}|\\d{2}|[2][0-4]\\d{1}|[2][5][0-5]|\\d{1})";
-    public String pattern=pattern0+"[.]"+pattern0+"[.]"+pattern0+"[.]"+pattern0;
-        }
-//Write your code here
+
